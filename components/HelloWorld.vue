21ce298f201b5f7c04ab54c5d54a4e5e409386f9
tkugimot <toshimitsu_kugimoto@yahoo.co.jp>
2019-06-23 17:58:13 +0900

    Add bulma

--- components/HelloWorld.vue
+++ components/HelloWorld.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>
-    {{ message }}
-  </div>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        Hello World
+      </h1>
+      <p class="subtitle">
+        My first website with <strong>Bulma</strong>!
+      </p>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
